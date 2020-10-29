5a1e5b9e7ffd799605cc936767cad51f6bdf648c
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2020-10-29 22:52:43 +0530

    Initial Commit - Skeleton created

---
+++ ProjectManagement.Api/ProjectManagement.Api/Controllers/ProjectController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectManagement.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.Api.Controllers
+{
+    [ApiController]
+    [Route("api/Project")]
+    public class ProjectController : BaseController<Project>
+    {
+    }
+}
---
+++ ProjectManagement.Api/ProjectManagement.Api/Controllers/TaskController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.Api.Controllers
+{
+    [ApiController]
+    [Route("api/Task")]
+    public class TaskController : BaseController<Task>
+    {
+    }
+}
